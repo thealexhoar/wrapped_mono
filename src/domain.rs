9ccf6ac25d261f070e4951e13e244f9d7494e454
Alex Hoar <thealexhoar@gmail.com>
2024-01-22 17:02:33 -0800

    added useful fns

--- src/domain.rs
+++ src/domain.rs
@@ -3,99 +3,101 @@ use crate::binds::{mono_domain_assembly_open, mono_domain_create, MonoDomain};
 /// Safe representation of [`MonoDomain`] type.
 #[derive(Eq, Clone, Copy)]
 pub struct Domain {
-    ptr: *mut MonoDomain,
+  ptr: *mut MonoDomain,
 }
 use std::ffi::CString;
 impl Domain {
-    /// Loads [`Assembly`] at path into domain, returns **None** if assembly could not be loaded(is missing or broken), and `Some(Assembly)` if it was successfully loaded.
-    #[must_use]
-    pub fn assembly_open(&self, path: &str) -> Option<Assembly> {
-        //! # Example
-        //!```no_run
-        //! # use wrapped_mono::*;
-        //! # let domain = jit::init("name",None);
-        //! let asm = domain.assembly_open("SomeAssembly.dll").expect("Could not load assembly!");
-        //!```
-        let cstr = CString::new(path).expect(crate::STR2CSTR_ERR);
-        let ptr = unsafe { mono_domain_assembly_open(self.get_ptr(), cstr.as_ptr()) };
-        if ptr.is_null() {
-            return None;
-        }
-        let _ = &cstr;
-        Some(unsafe { Assembly::from_ptr(ptr) })
+  /// Loads [`Assembly`] at path into domain, returns **None** if assembly could not be loaded(is missing or broken), and `Some(Assembly)` if it was successfully loaded.
+  #[must_use]
+  pub fn assembly_open(&self, path: &str) -> Option<Assembly> {
+    //! # Example
+    //!```no_run
+    //! # use wrapped_mono::*;
+    //! # let domain = jit::init("name",None);
+    //! let asm = domain.assembly_open("SomeAssembly.dll").expect("Could not load assembly!");
+    //!```
+    let cstr = CString::new(path).expect(crate::STR2CSTR_ERR);
+    let ptr = unsafe { mono_domain_assembly_open(self.get_ptr(), cstr.as_ptr()) };
+    if ptr.is_null() {
+      return None;
     }
-    /// Creates a new empty domain
-    /// # Example
-    /// ```no_run
-    /// # use wrapped_mono::*;
-    /// let domain1 = jit::init("name",None);
-    /// let domain2 = Domain::create();
-    /// ```
-    #[must_use]
-    pub fn create() -> Self {
-        unsafe { Self::from_ptr(mono_domain_create()) }
+    let _ = &cstr;
+    Some(unsafe { Assembly::from_ptr(ptr) })
+  }
+  /// Creates a new empty domain
+  /// # Example
+  /// ```no_run
+  /// # use wrapped_mono::*;
+  /// let domain1 = jit::init("name",None);
+  /// let domain2 = Domain::create();
+  /// ```
+  #[must_use]
+  pub fn create() -> Self {
+    unsafe { Self::from_ptr(mono_domain_create()) }
+  }
+  /// Sets domain confing to one loaded from file *filename* in directory *`base_directory`*.
+  pub fn set_config(&self, base_directory: &str, filename: &str) {
+    let bd_cstr = CString::new(base_directory).expect(crate::STR2CSTR_ERR);
+    let fnme_cstr = CString::new(filename).expect(crate::STR2CSTR_ERR);
+    unsafe {
+      crate::binds::mono_domain_set_config(self.ptr, bd_cstr.as_ptr(), fnme_cstr.as_ptr());
     }
-    /// Sets domain confing to one loaded from file *filename* in directory *`base_directory`*.
-    pub fn set_config(&self, base_directory: &str, filename: &str) {
-        let bd_cstr = CString::new(base_directory).expect(crate::STR2CSTR_ERR);
-        let fnme_cstr = CString::new(filename).expect(crate::STR2CSTR_ERR);
-        unsafe {
-            crate::binds::mono_domain_set_config(self.ptr, bd_cstr.as_ptr(), fnme_cstr.as_ptr());
-        }
-        drop(bd_cstr);
-        drop(fnme_cstr);
+    drop(bd_cstr);
+    drop(fnme_cstr);
+  }
+  /// Function creating [`Domain`] type from a pointer to [`MonoDomain`].
+  /// # Safety
+  /// Pointer must be a valid pointer to [`MonoDomain`].
+  pub unsafe fn from_ptr(ptr: *mut MonoDomain) -> Self {
+    Self { ptr }
+  }
+  /// Function returning internal pointer to [`MonoDomain`]
+  #[must_use]
+  pub fn get_ptr(&self) -> *mut MonoDomain {
+    self.ptr
+  }
+  /// Sets domain as the current domain.
+  pub fn set(&self, active: bool) {
+    unsafe { crate::binds::mono_domain_set(self.ptr, i32::from(active)) };
+  }
+  /// Attaches current thread (makes domain "aware" of this threads existence, allowing domain to eg. automatically stop it during garbage collection to prevent errors.) Should be done for all threads that will interact with this domain.  
+  pub fn attach_thread(&self) {
+    unsafe { crate::binds::mono_jit_thread_attach(self.ptr) };
+  }
+  /* TODO: fix domain unloading/freeing
+  /// [DOES not work]
+  fn unload(self){
+      self.set(true);
+      self.attach();
+      unsafe{crate::binds::mono_domain_unload(self.ptr)};
+      drop(self);
+  }
+  */
+  /// Releases resources related to a specific domain. If *force* is true, allows releasing of the root domain. Used during shut-down.
+  /// # Safety
+  /// Since this function releases all resources related to given domain, it means that all references to objects inside it will become invalid.
+  pub fn free(mut self, force: bool) {
+    unsafe {
+      crate::binds::mono_domain_free(self.ptr, force as i32);
+      self.ptr = std::ptr::null_mut();
+    };
+  }
+  /// Returns current domain or `None` if mono runtime is not initialized yet.
+  #[must_use]
+  pub fn get_current() -> Option<Self> {
+    let ptr = unsafe { crate::binds::mono_domain_get() };
+    if ptr.is_null() {
+      None
+    } else {
+      unsafe { Some(Self::from_ptr(ptr)) }
     }
-    /// Function creating [`Domain`] type from a pointer to [`MonoDomain`].
-    /// # Safety
-    /// Pointer must be a valid pointer to [`MonoDomain`].
-    pub unsafe fn from_ptr(ptr: *mut MonoDomain) -> Self {
-        Self { ptr }
-    }
-    /// Function returning internal pointer to [`MonoDomain`]
-    #[must_use]
-    pub fn get_ptr(&self) -> *mut MonoDomain {
-        self.ptr
-    }
-    /// Sets domain as the current domain.
-    pub fn set(&self, active: bool) {
-        unsafe { crate::binds::mono_domain_set(self.ptr, i32::from(active)) };
-    }
-    /// Attaches current thread (makes domain "aware" of this threads existence, allowing domain to eg. automatically stop it during garbage collection to prevent errors.) Should be done for all threads that will interact with this domain.  
-    pub fn attach_thread(&self) {
-        unsafe { crate::binds::mono_jit_thread_attach(self.ptr) };
-    }
-    /* TODO: fix domain unloading/freeing
-    /// [DOES not work]
-    fn unload(self){
-        self.set(true);
-        self.attach();
-        unsafe{crate::binds::mono_domain_unload(self.ptr)};
-        drop(self);
-    }
-    /// Releases resources related to a specific domain. If *force* is true, allows releasing of the root domain. Used during shut-down.
-    /// # Safety
-    /// Since this function releases all resources related to given domain, it means that all references to objects inside it will become invalid.
-    fn free(self,force:bool){
-        unsafe{crate::binds::mono_domain_free(self.ptr,force as i32)};
-        drop(self);
-    }
-    */
-    /// Returns current domain or `None` if mono runtime is not initialized yet.
-    #[must_use]
-    pub fn get_current() -> Option<Self> {
-        let ptr = unsafe { crate::binds::mono_domain_get() };
-        if ptr.is_null() {
-            None
-        } else {
-            unsafe { Some(Self::from_ptr(ptr)) }
-        }
-    }
+  }
 }
 // Allows you to compare two domains to check if they are one and the same.
 impl std::cmp::PartialEq for Domain {
-    fn eq(&self, other: &Self) -> bool {
-        self.ptr == other.ptr
-    }
+  fn eq(&self, other: &Self) -> bool {
+    self.ptr == other.ptr
+  }
 }
 // Domains are OK to share between threads
 unsafe impl Sync for Domain {}
--- src/jit.rs
+++ src/jit.rs
@@ -1,4 +1,7 @@
-use crate::binds::{mono_jit_cleanup, mono_jit_exec, mono_jit_init, mono_jit_init_version};
+use crate::binds::{
+  mono_debug_init, mono_jit_cleanup, mono_jit_exec, mono_jit_init, mono_jit_init_version,
+  mono_jit_parse_options, MonoDebugFormat_MONO_DEBUG_FORMAT_MONO,
+};
 use crate::domain::Domain;
 use std::ffi::CString;
 static mut HAS_BEEN_INITIALIZED: bool = false;
@@ -15,28 +18,28 @@ static mut HAS_BEEN_INITIALIZED: bool = false;
 /// # Panics
 /// Panics if the runtime is initialised second time. `MonoRuntime` can be only initialised once.
 pub fn init(name: &str, version: Option<&str>) -> Domain {
-    unsafe {
-        assert!(
-            !HAS_BEEN_INITIALIZED,
-            "Mono runtime can't be initialized twice in the same process."
-        );
-        HAS_BEEN_INITIALIZED = true;
-    }
-    let n_cstr = CString::new(name).expect(crate::STR2CSTR_ERR);
-    let res = unsafe {
-        Domain::from_ptr(version.map_or_else(
-            || mono_jit_init(n_cstr.as_ptr()),
-            |s| {
-                let v_cstr = CString::new(s).expect(crate::STR2CSTR_ERR);
-                let ptr = mono_jit_init_version(n_cstr.as_ptr(), v_cstr.as_ptr());
-                let _ = &v_cstr;
-                ptr
-            },
-        ))
-    };
-    unsafe { crate::binds::mono_jit_thread_attach(res.get_ptr()) };
-    let _ = &n_cstr;
-    res
+  unsafe {
+    assert!(
+      !HAS_BEEN_INITIALIZED,
+      "Mono runtime can't be initialized twice in the same process."
+    );
+    HAS_BEEN_INITIALIZED = true;
+  }
+  let n_cstr = CString::new(name).expect(crate::STR2CSTR_ERR);
+  let res = unsafe {
+    Domain::from_ptr(version.map_or_else(
+      || mono_jit_init(n_cstr.as_ptr()),
+      |s| {
+        let v_cstr = CString::new(s).expect(crate::STR2CSTR_ERR);
+        let ptr = mono_jit_init_version(n_cstr.as_ptr(), v_cstr.as_ptr());
+        let _ = &v_cstr;
+        ptr
+      },
+    ))
+  };
+  unsafe { crate::binds::mono_jit_thread_attach(res.get_ptr()) };
+  let _ = &n_cstr;
+  res
 }
 /// This function shuts down the `MonoRuntime`.
 /// **WARNING!** after it is called, `MonoRuntime` **will not be** able to be used again in the same process, since it can be only started up once.
@@ -47,7 +50,7 @@ pub fn init(name: &str, version: Option<&str>) -> Domain {
 /// jit::cleanup(main_domain);
 /// ```
 pub fn cleanup(domain: Domain) {
-    unsafe { mono_jit_cleanup(domain.get_ptr()) };
+  unsafe { mono_jit_cleanup(domain.get_ptr()) };
 }
 use crate::assembly::Assembly;
 /// Function used to call main function from assembly in domain with arguments.
@@ -69,30 +72,61 @@ use crate::assembly::Assembly;
 /// # Errors
 /// Returns err if could not convert an arg to a `CString`. The `err` will contain the index of the invalid argument.
 pub fn exec(domain: &Domain, assembly: &Assembly, args: Vec<&str>) -> Result<i32, usize> {
-    #[allow(clippy::cast_possible_truncation)]
-    #[allow(clippy::cast_possible_wrap)]
-    let argument_count: i32 = (args.len() + 1) as i32;
-    let mut cstr_args: Vec<CString> = Vec::new();
-    let mut argument_vector: Vec<*mut i8> = Vec::with_capacity(args.len() + 1);
-    // 1-st argument is expected to be assembly name
-    unsafe {
-        argument_vector.push(crate::binds::mono_stringify_assembly_name(
-            crate::binds::mono_assembly_get_name(assembly.get_ptr()),
-        ));
-    }
-    for (index, arg) in args.into_iter().enumerate() {
-        let Ok(cstr_arg) = CString::new(arg) else { return Err(index) };
-        argument_vector.push(cstr_arg.as_ptr() as *mut i8);
-        cstr_args.push(cstr_arg);
-    }
-    let res = unsafe {
-        mono_jit_exec(
-            domain.get_ptr(),
-            assembly.get_ptr(),
-            argument_count,
-            argument_vector.as_mut_ptr(),
-        )
+  #[allow(clippy::cast_possible_truncation)]
+  #[allow(clippy::cast_possible_wrap)]
+  let argument_count: i32 = (args.len() + 1) as i32;
+  let mut cstr_args: Vec<CString> = Vec::new();
+  let mut argument_vector: Vec<*mut i8> = Vec::with_capacity(args.len() + 1);
+  // 1-st argument is expected to be assembly name
+  unsafe {
+    argument_vector.push(crate::binds::mono_stringify_assembly_name(
+      crate::binds::mono_assembly_get_name(assembly.get_ptr()),
+    ));
+  }
+  for (index, arg) in args.into_iter().enumerate() {
+    let Ok(cstr_arg) = CString::new(arg) else {
+      return Err(index);
     };
-    let _ = &cstr_args;
-    Ok(res)
+    argument_vector.push(cstr_arg.as_ptr() as *mut i8);
+    cstr_args.push(cstr_arg);
+  }
+  let res = unsafe {
+    mono_jit_exec(
+      domain.get_ptr(),
+      assembly.get_ptr(),
+      argument_count,
+      argument_vector.as_mut_ptr(),
+    )
+  };
+  let _ = &cstr_args;
+  Ok(res)
+}
+
+pub fn parse_options(args: Vec<&str>) -> Result<(), usize> {
+  let arg_count = args.len();
+  let argc = arg_count as i32;
+  let mut cstr_args: Vec<CString> = Vec::new();
+  let mut args_vec: Vec<*mut i8> = Vec::with_capacity(arg_count);
+
+  unsafe {
+    for (index, arg) in args.into_iter().enumerate() {
+      let Ok(cstr_arg) = CString::new(arg) else {
+        return Err(index);
+      };
+      args_vec.push(cstr_arg.as_ptr() as *mut i8);
+      cstr_args.push(cstr_arg);
+    }
+  }
+  unsafe {
+    mono_jit_parse_options(argc, args_vec.as_mut_ptr());
+  }
+  let _ = &cstr_args;
+
+  Ok(())
+}
+
+pub fn debug_init() {
+  unsafe {
+    mono_debug_init(MonoDebugFormat_MONO_DEBUG_FORMAT_MONO);
+  }
 }
